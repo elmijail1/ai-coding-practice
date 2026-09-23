450dd2e3672820119213ecb508aad49864367421
elmijail1 <mteplyshev@gmail.com>
2026-09-23 15:36:59 +0400

    feat(data): add attributes and 1 more unit for each faction

--- backend/src/types/unit.ts
+++ backend/src/types/unit.ts
@@ -1,3 +1,21 @@
+const ATTRIBUTES = [
+  "air",
+  "armored",
+  "biological",
+  "detector",
+  "ground",
+  "heroic",
+  "light",
+  "massive",
+  "mechanical",
+  "melee",
+  "psionic",
+  "ranged",
+  "structure",
+  "summoned",
+] as const;
+type TUnitAttribute = (typeof ATTRIBUTES)[number];
+
 export type TUnit = {
   id: number;
   name: string;
@@ -6,5 +24,6 @@ export type TUnit = {
   basicDamage: number;
   cost: { minerals: number; vespene: number; supply: number };
   productionHotkey: string;
-  targets: "ground" | "air" | "ground and air";
+  targets: "ground" | "air" | "ground and air" | "none";
+  type: TUnitAttribute[];
 };
